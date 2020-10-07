88d27130cc61b3cb9493ee4c842e35932a9fcddb
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-10-06 20:05:48 -0600

    Add test to count cards in deck

--- test/deck-test.js
+++ test/deck-test.js
@@ -29,5 +29,16 @@ describe('Deck', function() {
     
     expect(deck.cards).to.have.lengthOf(4);
   })
+  
+  it('should count cards in deck', function() {
+  const card1 = new Card(1, 'What color is the sky', ['orange', 'green', 'blue'], 'blue');
+  const card2 = new Card(2, 'What month is Halloween in', ['October', 'December', 'June'], 'October');
+  const card3 = new Card(3, 'What animal has stripes', ['lion', 'zebra', 'cow'], 'zebra');
+  const card4 = new Card(4, 'When is breakfast eaten', ['morning', 'afternoon', 'night'], 'morning');
+
+  const deck = new Deck([card1, card2, card3, card4]);
+
+  expect(deck.countCards()).to.be.equal(4);
+})
 
 })
