e940f2e0f99b1619a7857e1b0ca41ca7e2716b2a
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-10-08 17:49:02 -0600

    Change percent correct to whole number

--- src/round.js
+++ src/round.js
@@ -29,7 +29,8 @@ class Round {
   calculatePercentCorrect() {
     let correct = this.turns - this.incorrectGuesses.length;
     let percent = (correct / this.turns) * 100
-    return percent;
+    let wholeNum = Math.floor(percent)
+    return wholeNum;
   }
   
   endRound() {
